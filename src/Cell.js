e9d149eb1468b6df870816b814aa66f41d3b08fc
BennyLouie <bennylouie1412@gmail.com>
2019-10-29 17:50:43 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -8,10 +8,16 @@ export default class Cell extends Component {
       color: this.props.color
     }
   }
+
+  handleClick = () => {
+    this.setState({
+      color: this.props.getSelectedColor()
+    })
+  }
   
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div onDragOver={this.handleClick} className="cell" style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
