1f21a1b8c719ca2a29488393b78915a91472808b
aibek <macbook@Macbooks-MBP.home>
2019-11-21 23:54:35 -0500

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 export default class Cell extends Component {
   
-  constructor(props) {
-    super(props)
-    this.state = {
-      color: this.props.color
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     color: this.props.color
+  //   }
+  // }
   
   render() {
     return (
